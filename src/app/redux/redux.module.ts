42c8e89ab82638ae849fdb41b1f55d3e8828bfe5
Jose Luis G <garciamartinezjoseluis@gmail.com>
2020-02-12 21:31:29 +0100

    add all neccesary in redux.module

--- src/app/redux/redux.module.ts
+++ src/app/redux/redux.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+
+// NgRx
 import { StoreModule } from '@ngrx/store';
-import { reducers, metaReducers } from './reducers';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreRouterConnectingModule } from '@ngrx/router-store'; // opcional
+import { StoreDevtoolsModule } from '@ngrx/store-devtools'; // opcional: instalar plugin chrome.
+
+// Environment
+import { environment } from '@environments/environment';
+
+import { ROOT_REDUCERS, metaReducers } from './reducers';
 
 
 
@@ -9,13 +19,21 @@ import { reducers, metaReducers } from './reducers';
   declarations: [],
   imports: [
     CommonModule,
-    StoreModule.forRoot(reducers, {
+    StoreModule.forRoot(ROOT_REDUCERS, {
       metaReducers,
       runtimeChecks: {
         strictStateImmutability: true,
-        strictActionImmutability: true
+        strictActionImmutability: true,
+        strictStateSerializability: true,
+        strictActionSerializability: true,
       }
-    })
+    }),
+    StoreDevtoolsModule.instrument({
+      name: 'NgRx APP*** Store App',
+      maxAge: 25, // Retains last 25 states
+      logOnly: environment.production, // Restrict extension to log-only mode
+    }),
+    EffectsModule.forRoot([]),
   ]
 })
 export class ReduxModule { }
