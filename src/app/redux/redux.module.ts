ea5ba1a16482c9ff3e2b9c6e96780e71ca16492a
Jose Luis G <garciamartinezjoseluis@gmail.com>
2020-02-12 20:59:36 +0100

    ng add @ngrx/store --module redux

--- src/app/redux/redux.module.ts
+++ src/app/redux/redux.module.ts
@@ -1,12 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { StoreModule } from '@ngrx/store';
+import { reducers, metaReducers } from '../reducers';
 
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    StoreModule.forRoot(reducers, {
+      metaReducers,
+      runtimeChecks: {
+        strictStateImmutability: true,
+        strictActionImmutability: true
+      }
+    })
   ]
 })
 export class ReduxModule { }
