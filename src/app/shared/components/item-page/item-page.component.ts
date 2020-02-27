74d8b9919a5ee0210acd4af2a168dd463d692968
Jose Luis G <garciamartinezjoseluis@gmail.com>
2020-02-27 14:13:55 +0100

    styles fix

--- src/app/shared/components/item-page/item-page.component.ts
+++ src/app/shared/components/item-page/item-page.component.ts
@@ -15,43 +15,18 @@ import * as searcher from '@shared/components/searcher/interfaces';
 })
 export class ItemPageComponent implements OnInit {
   searcher: searcher.Out;
-  appPages = [
-    {
-      title: 'Home',
-      url: '/home',
-      icon: 'calendar'
-    },
-    {
-      title: 'Hormigas',
-      url: '/ants',
-      icon: 'calendar'
-    }
-  ];
+
 
   constructor(private dataService: DataService) { }
 
   ngOnInit() {
-    console.log('init page');
-    this.dataService.loadItems();
+
   }
 
   // recibe del componente buscador y guarda el objeto en searcher.
   onSearch(out: searcher.Out) {
     this.searcher = out;
     console.log(out.value);
-  }
-
-
-  openCloseSidenav(event) {
-    // if (event.type === 'ionDidOpen') {
-    //   this.store.dispatch(LayoutActions.openSidenav());
-    // } else if (event.type === 'ionDidClose') {
-    //   this.store.dispatch(LayoutActions.closeSidenav());
-    // }
-  }
-
-  openTutorial() {
-    console.log('click');
   }
 
 }
