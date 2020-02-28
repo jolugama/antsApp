6145cc3f8a67130f011d03e39a7883649fc3b659
Jose Luis G <garciamartinezjoseluis@gmail.com>
2020-02-28 12:46:58 +0100

    refactor

--- src/app/shared/services/data.service.ts
+++ src/app/shared/services/data.service.ts
@@ -22,6 +22,7 @@ import { switchMap, map, tap, take, concatAll, debounceTime, distinctUntilChange
 export class DataService {
   items$: Observable<any>;
   route$: Observable<any>;
+  keyAnts: 'ants';
   constructor(
     public router: Router,
     private storeAnts$: Store<fromAntsReducers.State>,
@@ -45,7 +46,7 @@ export class DataService {
       // }),
       // recojo el observable, y como voy a devoler otro observable lo enmascaro con un switchMap
       switchMap((value: string) => {
-        if (value.split('/')[1].includes('ants')) {
+        if (this.isOnState(value, this.keyAnts)) {
           const temp = this.storeAnts$.pipe(
             select(fromAntsReducers.selectItemsSearch)
           );
@@ -55,10 +56,10 @@ export class DataService {
           return combineLatest(of(value), of({ items: [] }));
         }
       }),
-      distinctUntilChanged((a, b) => JSON.stringify(a) === JSON.stringify(b)),
+      distinctUntilChanged((a, b) => JSON.stringify(a[1].items) === JSON.stringify(b[1].items)),
       map((data) => {
         if (data[1].items.length === 0) {
-          if (data[0].split('/')[1].includes('ants')) {
+          if (this.isOnState(data[0], this.keyAnts)) {
             return this.storeAnts$.dispatch(fromAntsActions.ItemsActions.loadItems());
           }
         }
@@ -70,6 +71,15 @@ export class DataService {
   getItems() {
     // const obj = this.getUrl;
     // TODO falta completar
+  }
+
+  /**
+   *
+   * @param route Es el que contiene la ruta completa
+   * @param word Es la palabra clave raiz, que debe contener
+   */
+  private isOnState(route, word): boolean {
+    return route.split('/')[1].includes(word);
   }
 
 
